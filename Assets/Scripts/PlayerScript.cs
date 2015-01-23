db02800f6616dc4e6daa9ce36f0baffeb0bf7622
Roope Timonen <timonen.roope@gmail.com>
2015-01-23 20:43:14 +0200

    Sprites and shet

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
+	private float move;
+	private float horiz;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		move = 0.3f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		horiz = Input.GetAxis ("Horizontal");
+
+		if (Input.GetAxis("Horizontal") != 0)
+		    {
+			transform.position += Vector3.right * move * horiz;
+		}
+	/*	if (Input.GetButtonDown ("Jump")) {
+			rigidbody2D.velocity = Vector3(0,10,0);
+				}*/
 	}
 }

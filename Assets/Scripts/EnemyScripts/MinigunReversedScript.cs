a4755e4f0c170fd46754afb344ad00e5a14bdfd7
unknown <Oasis3@Oasis17-PC.minestudios.local>
2015-01-24 17:55:06 +0200

    MINIGUNS!!!!!

---
+++ Assets/Scripts/EnemyScripts/MinigunReversedScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinigunReversedScript : MonoBehaviour {
+	
+	public GameObject ammoPrefab;
+	public GameObject hitPrefab;
+	public float ammoInterwall;
+	
+	private float timer;
+	private Vector3 spawnPoint;
+	
+	private bool moveUp;
+	
+	public LayerMask mask = 8;
+	
+	public float speed = 1;
+	
+	
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		
+		timer = 0;
+		
+		
+		spawnPoint = new Vector3(transform.position.x - 0.35f, transform.position.y, transform.position.z);
+		
+		moveUp = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		timer += Time.deltaTime;
+		
+		spawnPoint = new Vector3(transform.position.x - 0.35f, transform.position.y + 0.1f, transform.position.z);
+		
+		if (timer > ammoInterwall)
+		{
+			
+			Instantiate(ammoPrefab, spawnPoint, Quaternion.Euler(0, -90, 180));
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.right, 20, mask.value);
+			
+			if (hit.collider != null)
+			{
+				
+				Instantiate(hitPrefab, hit.point, Quaternion.identity);
+				
+				if (hit.collider.tag == "Player")
+				{
+					//Whatever you want to happen to poor player
+				}
+			}
+			
+			timer = 0.0f;
+		}
+		
+		
+		RaycastHit2D upwards = Physics2D.Raycast(transform.position, Vector2.up, 1, mask.value);
+		
+		if (upwards.collider != null)
+		{
+			moveUp = false;
+		}
+		
+		RaycastHit2D downwards = Physics2D.Raycast(transform.position, -Vector2.up, 1, mask.value);
+		
+		if (downwards.collider != null)
+		{
+			moveUp = true;
+		}
+		
+		if(moveUp)
+		{
+			transform.Translate(Vector2.up * speed * Time.deltaTime);
+		}
+		else 
+		{
+			transform.Translate(-Vector2.up * speed * Time.deltaTime);
+		}
+	
+	}
+}

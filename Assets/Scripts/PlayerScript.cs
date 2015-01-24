317ef2501471b45f670df3ad6d24749aa8fe5712
Harri Karhu <harri.karhu@iki.fi>
2015-01-25 00:34:41 +0200

    Player dynamics fixed

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,44 +1,54 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-	private float move;
-	private float horiz;
+	public float horiz;
 	public bool grounded;
+	//public bool doubleJumped;
+	public float groundMultiplier;
+	public float flightMultiplier;
+	public float jumpForce;
+	//public float doubleJumpForce;
+	public float momentum;
 
 	// Use this for initialization
 	void Start () {
-
-		move = 10f;
 		grounded = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		horiz = Input.GetAxis ("Horizontal");
+		horiz = horiz*(1-momentum) + Input.GetAxis ("Horizontal")*(momentum);
 
-		if (Input.GetAxis("Horizontal") != 0 && grounded)
-		    {
-			rigidbody2D.AddForce(new Vector2(10,0) * horiz * move);
-
-			//transform.position += Vector3.right * move * horiz * Time.deltaTime;
+		if (grounded){
+			transform.position += (new Vector3 (1, 0, 0)) * horiz * Time.deltaTime * groundMultiplier;
+		} else {
+			transform.position += (new Vector3 (1, 0, 0)) * horiz * Time.deltaTime * flightMultiplier;
 		}
-		if (Input.GetButtonDown("Jump") && grounded)
-		{
-			rigidbody2D.AddForce(new Vector2(0,10),ForceMode2D.Impulse);
+
+		if (Input.GetButtonDown("Jump") && grounded){
+			rigidbody2D.AddForce(new Vector2(horiz*flightMultiplier,jumpForce),ForceMode2D.Impulse);
 			grounded = false;
 		}
+		//else if(Input.GetButtonDown("Jump") && !doubleJumped){
+		//	rigidbody2D.AddForce(new Vector2(horiz,doubleJumpForce),ForceMode2D.Impulse);
+		//	doubleJumped = true;
+		//}
+
+		//Lol, reset :D
+		if (transform.position.y < -100) {
+			transform.position = new Vector3(0,0,0);
+		}
+
 	}
 	void OnTriggerStay2D(Collider2D col){
 		if (col.gameObject.tag == "Ground") {
-						grounded = true;
-				} else
-						grounded = false;
+			if(grounded == false) horiz = 0; //Don't keep momentum when touching ground first time after jump.
+			grounded = true;
+			//doubleJumped = false;
+		} else
+			grounded = false;
 	}
-	void OnTriggerExit2D(Collider2D col){
-		if (col.gameObject.tag == "Ground") {		
-						grounded = false;
-				}
-		}
 }

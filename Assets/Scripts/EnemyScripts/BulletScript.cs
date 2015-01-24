3d9741e5fbd6d3d399c17bdd25c8e4753def2307
unknown <Oasis3@Oasis17-PC.minestudios.local>
2015-01-24 16:58:20 +0200

    Whole new set of enemies!

---
+++ Assets/Scripts/EnemyScripts/BulletScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+		Destroy(gameObject, 5f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate(Vector2.right * speed * Time.deltaTime);
+	
+	}
+}
---
+++ Assets/Scripts/EnemyScripts/TurretReversedScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretReversedScript : MonoBehaviour {
+	
+	public GameObject ammoPrefab;
+	public GameObject hitPrefab;
+	public float ammoInterwall;
+	
+	private float timer;
+	private Vector3 spawnPoint;
+	
+	private Animator anim;
+	
+	public LayerMask mask = 8;
+
+	// Use this for initialization
+	void Start () {
+		
+		anim = GetComponent<Animator>();
+		timer = 0;
+		
+		spawnPoint = new Vector3(transform.position.x - 0.8f, transform.position.y + 0.3f, transform.position.z);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		timer += Time.deltaTime;
+		
+		if (timer > ammoInterwall)
+		{
+			anim.SetTrigger("Shoot");
+			Instantiate(ammoPrefab, spawnPoint, Quaternion.Euler(0, -90, 180));
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.right, 20, mask.value);
+			
+			if (hit.collider != null)
+			{
+				
+				Instantiate(hitPrefab, hit.point, Quaternion.identity);
+				
+				if (hit.collider.tag == "Player")
+				{
+					//Whatever you want to happen to poor player
+				}
+			}
+			timer = 0.0f;
+		}
+	
+	}
+}

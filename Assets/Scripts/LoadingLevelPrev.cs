cc50214e9f10e0a683bc571f9d96997064ec4a99
unknown <Oasis3@Oasis17-PC.minestudios.local>
2015-01-25 02:11:29 +0200

    Added next and previous level loading

---
+++ Assets/Scripts/LoadingLevelPrev.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingLevelPrev : MonoBehaviour {
+	int i;
+	public BoxCollider2D box;
+	
+	// Use this for initialization
+	void Start () {
+		i = 0;
+	print (Application.loadedLevel);
+		box = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		i = Application.loadedLevel - 1;
+		Application.LoadLevel (i);
+		//box.enabled = false;
+	}
+}
